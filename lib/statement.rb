0b200b4b28b3efb01e672c4771b98f7ac4901bb9
bookloverbabe <solmaz@hotmail.co.uk>
2023-05-22 15:17:24 +0100

    Passed second test

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,10 +1,16 @@
 class Statement
+# return the date || credit || debit || balance.
+# statement is comprised of key, the date, and the value, amount and balance
   def initialize
     @balance = []
   end
 
-  def balance(amount)
+  def balance
+    return @balance.sum
+  end
+  
+  # Credit is a deposit
+  def credit(amount)
     return @balance.sum(amount)
   end
-
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,6 +4,14 @@ RSpec.describe Statement do
   it "initializes" do
     statement = Statement.new
     # The balance returns 0
-    expect(statement.balance(0)).to eq(0)
+    expect(statement.balance).to eq(0)
   end
+
+  context "returns the balance after user deposits a sum" do
+    it "user deposits £5" do
+      statement = Statement.new
+      expect(statement.credit(5)).to eq(5)
+    end
+  end
+
 end
