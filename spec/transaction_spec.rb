eecd4fa0c22d27c50c01b05f4f931c21a047d4ac
bookloverbabe <solmaz@hotmail.co.uk>
2023-05-23 16:29:58 +0100

    Created transaction class and passed all tests

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -4,7 +4,16 @@ RSpec.describe Transaction do
   describe "initilize" do
     transaction = Transaction.new('12-12-2012', 5, 10, 15)
       it "has a date" do
-        expect(transaction.date). to eq('12-12-2012')
+        expect(transaction.date).to eq('12-12-2012')
+      end
+      it "contains a credit amount" do
+        expect(transaction.credit).to eq(5)
+      end
+      it "contains a debit amount" do
+        expect(transaction.debit).to eq(10)
+      end
+      it "contains a balance amount" do
+        expect(transaction.balance).to eq(15)
       end
   end
 end
