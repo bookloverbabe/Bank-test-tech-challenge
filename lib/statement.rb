0cfbb19d2672fc85e01b47f3bc5d5d46200939e0
bookloverbabe <solmaz@hotmail.co.uk>
2023-05-24 16:09:25 +0100

    Final commit

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,15 +1,14 @@
 # This class explains the format of the statement. The format
 # Takes the bank_statement instance variable from account class which contains the data, and formats it
 # Format = "date || credit || debit || balance\n"
-DIVIDER = "||"
 
 class Statement
   def print_statement(bank_statement)
-    # print_heading
-    bank_statement.each { |transaction| puts transaction.date + "||" + transaction.credit + "||" +  transaction.debit + "||" + transaction.balance }
+    print_heading
+    bank_statement.each { |transaction| puts transaction.date + "||" + transaction.credit('%#.2f') + "||" +  transaction.debit('%#.2f') + "||" + transaction.balance('%#.2f')}
+  end
+
+  def print_heading
+    puts 'date || credit || debit || balance'
   end
 end
-
-  # def print_heading
-  #   puts 'date || credit || debit || balance'
-  # end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,11 +1,23 @@
 require 'statement'
 RSpec.describe "Statement" do
   describe "print_statement" do
-  let(:transaction1) { double(:transaction, date: '12-12-2012', credit: 5, debit: 10, balance: 15) }
-    before do
-      statement = Statement.new
-      bank_statement = [transaction1]
-      allow(statement).to receive(statement.print_statement(transaction1)).and_return('12-12-2012 || 5 || 10 || 15')
+    let(:transaction1) {double( date: '12-12-2012', credit: 5.00, debit: 10.00, balance: 15.00) }
+    let(:transaction2) {double( date: '24-05-2023', credit: 1000.00, debit: 10.00, balance: 990.00) }
+    context "checks the output" do
+      before do
+        statement = Statement.new
+        print_heading = 'date || credit || debit || balance'
+        result = [transaction1] + print_heading
+        allow(statement).to receive(statement.print_statement(transaction1)).and_return(result)
+      end
+    end
+    context "checks the output again" do
+      before do
+        statement = Statement.new
+        print_heading = 'date || credit || debit || balance'
+        result = [transaction2] + print_heading
+        allow(statement).to receive(statement.print_statement(transaction1)).and_return(result)
+      end
     end
   end
 end
